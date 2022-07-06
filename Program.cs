dbfc01160b82b4d603061a23becf93b14003caa0
throatbeard <fos99006@byui.edu>
2022-07-06 10:39:03 -0600

    Copied Prgram and Constants

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Unit06.Game.Directing;
+using Unit06.Game.Services;
+
+namespace Unit06
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director(SceneManager.VideoService);
+            director.StartGame();
+        }
+    }
+}
