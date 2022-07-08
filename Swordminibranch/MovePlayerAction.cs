e44b50dd201a451ff2d5e52c498bc2e3b5a5f7d1
Mark <markc7856@yahoo.com>
2022-07-08 15:13:07 -0600

    created a mini branch starting with example code instead of brick

---
+++ Swordminibranch/MovePlayerAction.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using Byui.Games.Casting;
+using Byui.Games.Scripting;
+using Byui.Games.Services;
+
+
+namespace Example.Scrolling
+{
+    /// <summary>
+    /// Moves the player within the game world while scrolling the screen.
+    /// </summary>
+    public class MovePlayerAction : Byui.Games.Scripting.Action
+    {
+        public MovePlayerAction()
+        {
+        }
+
+        public override void Execute(Scene scene, float deltaTime, IActionCallback callback)
+        {
+            try
+            {
+                // get the actors, including the camera, from the cast
+                Camera camera = scene.GetFirstActor<Camera>("camera");
+                Actor world = camera.GetWorld();
+                Actor player = scene.GetFirstActor("player");
+                Actor enemy = scene.GetFirstActor("enemy");
+                
+                // move the player and clamp it to the boundaries of the world.
+                player.Move();
+                enemy.Move();
+                player.ClampTo(world);
+                enemy.ClampTo(world);    
+            }
+            catch (Exception exception)
+            {
+                callback.OnError("Couldn't move player.", exception);
+            }
+        }
+    }
+}
